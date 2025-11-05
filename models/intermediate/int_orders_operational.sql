540dc1ada5768afb0552176b95f4572dea6f79dc
Christopher te Brake <christopher.tebrake@gmail.com>
2025-11-05 13:57:57 +0000

    added: mart/finance_days: with daily granularity of     Date     Total number of transactions     Total revenue     Average Basket     Operational Margin     Total purchase cost     Total shipping fees     Total log costs     Total quantity of products sold.

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -6,6 +6,11 @@ select
 ,   ROUND((SUM(orders.margin) + SUM(ship.shipping_fee) - SUM(ship.logcost) - SUM(ship.ship_cost)),2) as operational_margin
 ,   SUM(quantity) as quantity
 ,   SUM(revenue) as revenue
+,   SUM(shipping_fee) as shipping_fee
+,   SUM(ship_cost)    as ship_cost
+,   SUM(logcost)      as logcost
+,   SUM(purchase_cost) as purchase_cost
+
     
 from {{ ref('int_orders_margin') }} as orders
 JOIN {{ ref('stg_raw__ship') }}     as ship
---
+++ models/mart/finance_days.sql
@@ -0,0 +1,30 @@
+/* 
+The finance team has communicated that they want the following data at a daily granularity:
+
+    Date
+    Total number of transactions
+    Total revenue
+    Average Basket
+    Operational Margin
+    Total purchase cost
+    Total shipping fees
+    Total log costs
+    Total quantity of products sold.
+*/
+
+select
+
+        date_date
+    ,   COUNT(distinct orders_id)                           as Total_number_transactions
+    ,   ROUND(SUM(revenue),2)                               as Total_revenue
+    ,   ROUND(SUM(revenue)/(COUNT(distinct orders_id)),2)   as Average_Basket
+    ,   ROUND(SUM(operational_margin),2)                             as total_operational_margin
+    ,   ROUND(SUM(purchase_cost),2)                                  as total_purchase_cost
+    ,   ROUND(SUM(shipping_fee),2)                                   as total_shipping_fee
+    ,   ROUND(SUM(logcost),2)                                        as total_log_cost
+    ,   SUM(quantity)                                      as total_quantity_of_products_sold
+
+from {{ ref('int_orders_operational') }}
+
+GROUP by date_date
+Order by date_date 
