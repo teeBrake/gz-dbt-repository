09457d0e5a9f3cbfd6ed2923e3dbad0d85610624
Christopher te Brake <christopher.tebrake@gmail.com>
2025-11-05 13:35:24 +0000

    added intermediate model int_orders_operational to calculate operational margin per order

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,14 @@
+-- 3rd intermediate model
+
+select
+    orders.orders_id
+,   MIN(orders.date_date) as date_date
+,   ROUND((SUM(orders.margin) + SUM(ship.shipping_fee) - SUM(ship.logcost) - SUM(ship.ship_cost)),2) as operational_margin
+,   SUM(quantity) as quantity
+,   SUM(revenue) as revenue
+    
+from {{ ref('int_orders_margin') }} as orders
+JOIN {{ ref('stg_raw__ship') }}     as ship
+USING(orders_id)
+Group by orders_id
+order by orders_id desc
